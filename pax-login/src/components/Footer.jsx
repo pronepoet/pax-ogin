fd38e5e9946c27df6c34479a1bbbef200df87498
Phillip <ochiengphillip84@gmail.com>
2023-11-20 05:10:48 +0300

    💄 Add a footer content

--- pax-login/src/components/Footer.jsx
+++ pax-login/src/components/Footer.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import './Footer.css'
+import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <>
     <div className="footer">
         <div className="container-fluid">
-            <button>FAQ Help Center</button>
+            <button className="helpButton"> <span class="material-symbols-outlined">
+help</span>  FAQ & Help Center</button>
+          
+            <button className="darkModeButton"><span class="material-symbols-outlined">Dark_mode</span>Dark theme</button>
         </div>
+<div className="links">
+  <div className="link-container">
+    <div className="level1">
+      <div className="level2">
+        <div className="level3">
+            <p>For You</p>
+            <nav>
+              <ul>
+                <li>Buy Bitcoin </li>
+                <li>Buy Tether</li>
+                <li>Sell Bitcoin </li>
+                <li>Sell Tether</li>
+                <li> Become Vendor</li>
+                <li>Paxful Wallet</li>
+              </ul>
+            </nav>
+        </div>
+      </div>
+    </div>
+
+  </div>
+ 
+</div>
+
     </div>
     </>
     
