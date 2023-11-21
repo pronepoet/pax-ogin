ba850142dc0e95c2d4568ec199b50fce42445f78
Phillip <ochiengphillip84@gmail.com>
2023-11-21 14:14:41 +0300

    💄: Add more styling to the footer component

--- pax-login/src/components/Footer.jsx
+++ pax-login/src/components/Footer.jsx
@@ -5,36 +5,40 @@ export const Footer = () => {
   return (
     <>
     <div className="footer">
+
+
         <div className="container-fluid">
+          <div className="footer-header">
             <button className="helpButton"> <span class="material-symbols-outlined">
-help</span>  FAQ & Help Center</button>
-          
+              help</span>  FAQ & Help Center</button>
             <button className="darkModeButton"><span class="material-symbols-outlined">Dark_mode</span>Dark theme</button>
-        </div>
-<div className="links">
-  <div className="link-container">
-    <div className="level1">
-      <div className="level2">
-        <div className="level3">
-            <p>For You</p>
-            <nav>
-              <ul>
-                <li>Buy Bitcoin </li>
-                <li>Buy Tether</li>
-                <li>Sell Bitcoin </li>
-                <li>Sell Tether</li>
-                <li> Become Vendor</li>
-                <li>Paxful Wallet</li>
-              </ul>
-            </nav>
-        </div>
-      </div>
-    </div>
+          </div>
 
-  </div>
- 
-</div>
 
+            <div className="links">
+
+              
+              <div className="link-container">
+                <div className="level1">
+                  <div className="level2">
+                    <p>For You</p>
+
+                    <nav>
+                      <ul>
+                        <li>
+                        Buy Bitcoin </li> <br />
+                        <li>Buy Tether</li><br />
+                        <li>Sell Bitcoin </li><br />
+                        <li>Sell Tether</li><br />
+                        <li> Become Vendor</li><br />
+                       <li>Paxful Wallet</li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
     </>
     
