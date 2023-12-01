45d3a51536d1a73b19e39f4895be93e90dff76fe
Phillip <ochiengphillip84@gmail.com>
2023-12-01 17:55:56 +0300

    Modify footer

--- pax-login/src/components/Footer.jsx
+++ pax-login/src/components/Footer.jsx
@@ -65,10 +65,170 @@ export const Footer = () => {
                   </div>
 
                 </div>
+              {/* second group */}
+
+              <div className="level1">
+                  <div className="level2">
+                    <p className="for-you">For You</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                        <li className="f-y-li">
+                          <a href="#" className="li-link">
+                          Buy Bitcoin
+                          </a>
+                         </li> <br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link"> Buy Tether</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Sell Bitcoin </a>
+                        </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Sell Tether </a></li><br />
+                        <li className="f-y-li"> 
+                        <a href="#" className="li-link">Become Vendor</a> </li><br />
+                       <li className="f-y-li">
+                       <a href="#" className="li-link">Paxful Wallet </a></li>
+                      </ul>
+                    </nav>
+                  </div>
+
+                  <div className="level2">
+                    <p className="for-you">For Your Business</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                      
+                        <li className="f-y-li">
+                        <a href="#" className="li-link"> Paxful Pay</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Virtual Bitcoin Kiosk</a>
+                        </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">API Documentation</a></li><br />
+                      </ul>
+                    </nav>
+                  </div>
+
+
               </div>
+
+{/* second group */}
+<div className="level1">
+                  <div className="level2">
+                    <p className="for-you">Growth</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                        <li className="f-y-li">
+                          <a href="#" className="li-link">
+                          Buy Bitcoin
+                          </a>
+                         </li> <br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Paxful Rewards Program</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Paxful Trader Program </a>
+                        </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Paxful Peer Program </a></li><br />
+                        <li className="f-y-li"> 
+                        <a href="#" className="li-link">Community</a> </li><br />
+                    
+                      </ul>
+                    </nav>
+                  </div>
+
+                  <div className="level2">
+                    <p className="for-you">Buy Anywhere</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                      
+                        <li className="f-y-li">
+                        <a href="#" className="li-link"> Buy Bitcoin in USA</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Virtual Bitcoin Kiosk</a>
+                        </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">API Documentation</a></li><br />
+                      </ul>
+                    </nav>
+                  </div>
+
+                  </div>
+
+{/* third group */}
+
+<div className="level1">
+                  <div className="level2">
+                    <p className="for-you">Useful Links</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                        <li className="f-y-li">
+                          <a href="#" className="li-link">
+                         Paxful Status
+                          </a>
+                         </li> <br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link"> Bitcoin Calculator</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Peer-to-Peer Market Prices </a>
+                        </li>
+                       
+                      </ul>
+                    </nav>
+                  </div>
+
+                  <div className="level2">
+                    <p className="for-you">About Paxful</p>
+
+                    <nav className="nav-group-1">
+                      <ul className="f-y">
+                      
+                        <li className="f-y-li">
+                        <a href="#" className="li-link"> About Us</a>
+                         </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Business Contacts</a>
+                        </li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Careers</a></li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Paxful Blog</a></li><br />
+                        <li className="f-y-li">
+                        <a href="#" className="li-link">Paxful Reviews</a></li><br />
+                      </ul>
+                    </nav>
+                  </div>
+                  </div>
+
+                  <div className="legal">
+                    <p className="legal-label">Legal</p>
+                    <nav className='legal-links'>
+                      <ul className='legal-ul'>
+                        <li className='legal-li'>Terms & Conditions</li>
+                        <li className='legal-li'>Vendor Reminder</li>
+                        <li className='legal-li'>AML Policy</li>
+                        <li className='legal-li'>Stable Terms of Service</li>
+                        <li className='legal-li'>Privacy Notice</li>
+                        <li className='legal-li'>Big Bounty Police</li>
+                        <li className='legal-li'>Cookie Policy</li>
+                        <li className='legal-li'>Restricted Countries</li>
+                      </ul>
+                    </nav>
+                  </div>
+
             </div>
         </div>
     </div>
+    /</div>
     </>
     
   )
