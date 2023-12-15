e785dae6473d68cdca98144f29a30917ea8fed09
Phillip <ochiengphillip84@gmail.com>
2023-12-15 18:59:59 +0300

    💄Styling updates

--- pax-login/src/App.js
+++ pax-login/src/App.js
@@ -1,18 +1,28 @@
 
 import './App.css';
-import { Link } from 'react-router-dom';
+
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
+import { Login } from './components/Login';
+import { Link, Routes, Route, Switch} from "react-router-dom";
 function App() {
   return (
     <>
+
+
+
       <Navbar />
+
       <div className='main'>
       <div className="container">
         <h2 className='container-header1'>SOMETHING'S MISSING HERE </h2>
         <h3 className='container-header2'>Error 404: We can't seem to find what you're looking for</h3>
       
         <p className="list-header">Check out the helpful links below</p>
+  
+
+
+        
        
           <ul className="list">
             <li>
@@ -31,14 +41,20 @@ function App() {
               <Link to="/">Support FAQ</Link>
             </li>
           </ul>
-      
-        
+
+
+ <Routes>
+ <Route path="/" component={App} />
+  <Route path="/Login" element={<Login />} />
+ </Routes>
+
+ 
       </div>
       </div>
    
 <Footer />
+
     </>
   );
 }
-
 export default App;
