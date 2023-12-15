66c3a1c4d8069d880f09f3fdfb30ced60267205a
Phillip <ochiengphillip84@gmail.com>
2023-12-15 17:32:20 +0300

    ✨Add styling to the navbar

--- pax-login/src/components/Navbar.jsx
+++ pax-login/src/components/Navbar.jsx
@@ -7,39 +7,35 @@ export const Navbar = () => {
       <div className="nav-cont">
       <nav className="nav-left">
       <ul className='nav-start'>
-        <li>
+        <li className="nav-li">
           <Link to="/">Buy</Link>
         </li>
-        <li>
+        <li  className="nav-li">
           <Link to="/Login">Sell</Link>
         </li>
-        <li>
-          <Link to="/contact">Create an Offer</Link>
+        <li  className="nav-li">
+          <Link className="nav-offer" to="/contact">Create an Offer</Link>
         </li>
-        <li>
+        <li  className="nav-li">
           <Link to="/contact">Wallet</Link>
         </li>
-        <li>
+        <li  className="nav-li">
           <Link to="/contact">Gift Card hub</Link>
         </li>
-        <li>
+        <li  className="nav-li">
           <Link to="/contact">Become a Vendor</Link>
         </li>
-     
-      
       </ul>
     </nav>
     <nav className='nav-cta'>
       <ul className="nav-ul">
-        <li>
-          <a href="#">Login</a>
-        </li>
-        <li>
-          <a href="#">Register</a>
-        </li>
+        <div className="nac-cta">
+        <a className="nav-a"href="#">Login</a>
+        <a className="wbutton" href="#">Register</a>
+        </div>
       </ul>
     </nav>
-      </div>
+    </div>
     </div>
   )
 }
